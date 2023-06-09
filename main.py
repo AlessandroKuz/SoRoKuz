9a0c6189ada9907ec1bbf0b01d40b4318b610827
Leonardo Rocca <leo.rocca03@gmail.com>
2023-06-09 14:41:24 +0200

    Create base structure and idea files

---
+++ main.py
@@ -0,0 +1,26 @@
+# Objective:
+# 1. Create base structure of the game
+# 2. Display the game grid
+# 3. Insert the numbers in the grid
+# 4. Check if the number is valid
+
+def display_grid():
+    ...
+
+
+def insert_numbers():
+    ...
+
+
+def check_numbers():
+    ...
+
+
+def main():
+    display_grid()
+    insert_numbers()
+    check_numbers()
+
+
+if __name__ == "__main__":
+    main()
