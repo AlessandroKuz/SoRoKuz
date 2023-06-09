d61b80f3d80301a2fd68ace9d7310bed1a2cb958
Elena Sorrentino <e.sorrentino@itsrizzoli.it>
2023-06-09 15:12:17 +0200

    Create the display_grid function
    Create the function that allows you to display the grid.

--- main.py
+++ main.py
@@ -4,8 +4,17 @@
 # 3. Insert the numbers in the grid
 # 4. Check if the number is valid
 
-def display_grid():
-    ...
+def display_grid(grid):
+    for i in range(len(grid)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - -")
+        for j in range(len(grid[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+            if j == 8:
+                print(grid[i][j])
+            else:
+                print(str(grid[i][j]) + " ", end="")
 
 
 def insert_numbers():
